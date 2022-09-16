797a44542397f21c7955118be557fcd5382df8fa
Braxton Ward <bward@grio.com>
2022-09-16 08:57:28 -0800

    Initial Commit

---
+++ Module5Lesson7/Module5Lesson7App.swift
@@ -0,0 +1,17 @@
+//
+//  Module5Lesson7App.swift
+//  Module5Lesson7
+//
+//  Created by Braxton Ward on 9/16/22.
+//
+
+import SwiftUI
+
+@main
+struct Module5Lesson7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
